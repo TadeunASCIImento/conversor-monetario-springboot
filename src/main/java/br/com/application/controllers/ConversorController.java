0541664fa4d45238d5cca9db87726f4b0717c7ba
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-07-23 14:10:41 -0300

    Ajustando a URI.

--- src/main/java/br/com/application/controllers/ConversorController.java
+++ src/main/java/br/com/application/controllers/ConversorController.java
@@ -2,6 +2,7 @@ package br.com.application.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,15 +10,15 @@ import org.springframework.web.servlet.ModelAndView;
 import br.com.application.models.Xml;
 
 @Controller
+@RequestMapping("/")
 public class ConversorController {
-
 
 	@Autowired
 	private RestTemplate restTemplate;
 
 	final String uriBase = "https://economia.awesomeapi.com.br/xml/";
 
-	@RequestMapping("/conversor")
+	@GetMapping
 	public ModelAndView converter() {
 		ModelAndView modelAndView = new ModelAndView("index");
 		Xml euro = restTemplate.getForObject(uriBase.concat("EUR"), Xml.class);
@@ -28,6 +29,5 @@ public class ConversorController {
 		modelAndView.addObject("turismo", turismo);
 		return modelAndView;
 	}
-
 
 }
