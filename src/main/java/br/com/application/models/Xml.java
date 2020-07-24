b81422649baa43e5d8bf988288516c75a36b27ad
TadeunASCIImento <tadeunascimento36@gmail.com>
2020-07-24 08:33:58 -0300

    Organizando imports.

--- src/main/java/br/com/application/models/Xml.java
+++ src/main/java/br/com/application/models/Xml.java
@@ -7,7 +7,6 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
